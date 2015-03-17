553714be226bc5b3bfee6bded479e755f9ec8215
steven ferrell <ferr.mail@gmail.com>
2015-03-17 13:13:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/images')));
+app.use(express.static(path.join(__dirname, 'public/stylesheets')));
+app.use(express.static(path.join(__dirname, 'public/javascript')));
 
 app.use('/', routes);
 app.use('/users', users);
