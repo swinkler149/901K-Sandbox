63bca0230de098ef53b51404a3cfd9747854c7c5
Ubuntu <ed@edhollingsworth.com>
2015-03-18 19:05:55 +0000

    New controller and view for map display

---
+++ source/c3/app/scripts/controllers/map.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name c3App.controller:MapCtrl
+ * @description
+ * # MapCtrl
+ * Controller of the c3App
+ */
+angular.module('c3App')
+  .controller('MapCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.myName = 'Ed';
+
+
+
+
+  });
