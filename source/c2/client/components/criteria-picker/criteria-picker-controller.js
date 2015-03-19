91cbe8cd8b5388600833523b4f6bdbec20a06596
steven ferrell <ferr.mail@gmail.com>
2015-03-19 13:04:46 -0400

    Update criteria-picker-controller.js

--- source/c2/client/components/criteria-picker/criteria-picker-controller.js
+++ source/c2/client/components/criteria-picker/criteria-picker-controller.js
@@ -4,8 +4,8 @@ angular.module('c2App')
   .controller('CriteriaPickerCtrl', function ($scope, $location) {
     
     //fill these up with data from server
-    $scope.employmentTypeData = [];
-    $scope.agriculturalClassData = [];
+    $scope.employmentTypeData = {}
+    $scope.agriculturalClassData = {};
 
     $scope.correlate = function() {
       //using criteria picker inputs
