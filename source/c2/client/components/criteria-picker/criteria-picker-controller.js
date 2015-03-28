20f7ad7870ff70e26258901dbcd957cd91b73233
steven ferrell <ferr.mail@gmail.com>
2015-03-28 18:49:41 -0400

    Added Empl Code

--- source/c2/client/components/criteria-picker/criteria-picker-controller.js
+++ source/c2/client/components/criteria-picker/criteria-picker-controller.js
@@ -4,11 +4,13 @@ angular.module('c2App')
   .controller('CriteriaPickerCtrl', function ($scope, $location, $http, socket) {
     // Call server APIs to populate collections
     $scope.agriculturalClassData = [];
-
     $http.get('/api/cropharvests/distinct/Commodity').success(function(agriculturalClassData) {
       $scope.agriculturalClassData = agriculturalClassData;
     });
     
     //fill these up with data from server
     $scope.employmentTypeData = [];
+    $http.get('/api/empl/distinct/OCC_TITLE').success(function(employmentTypeData) {
+      $scope.employmentTypeData = employmentTypeData;
+    })
   });
