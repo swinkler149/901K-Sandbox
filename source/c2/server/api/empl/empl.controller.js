c6697351d2d2daee18c204914c3a723d0161d0fb
steven ferrell <ferr.mail@gmail.com>
2015-03-28 18:50:26 -0400

    Added Empl Code

--- source/c2/server/api/empl/empl.controller.js
+++ source/c2/server/api/empl/empl.controller.js
@@ -5,9 +5,26 @@ var Empl = require('./empl.model');
 
 // Get list of empls
 exports.index = function(req, res) {
-  Empl.find(function (err, emplCats) {
+	
+  Empl.count({ "OCC_TITLE" : req.params.empl }, function (err, count) {
+    if (err) return handleError(err);
+    console.log('cropharvest.controller: There are %d records matching the criteria', count);
+  });
+  
+  // find each crop record with the given parameters, selecting the identified columns
+  Empl.find({ "OCC_TITLE" : req.params.empl }, 'STATE OCC_TITLE TOT_EMP', function (err, empls) {
     if(err) { return handleError(res, err); }
-    return res.json(200, emplCats);
+    return res.json(200, empls);
+  });
+};
+
+// Get list of distinct Commodity values
+exports.distinct = function(req, res) {
+  console.log('empl.controller: Received: field='+req.params.field);
+  Empl.distinct( req.params.field, null, function (err, distinctEmpls) {
+    if (err) return handleError(err);
+    // Continue if there are no errors...
+    return res.json(200, distinctEmpls);
   });
 };
 
